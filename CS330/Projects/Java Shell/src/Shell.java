8613d709a9e02465c1e062e73a810f9174f5af2b
Nick Alexander <alexander50@live.com>
2014-09-19 17:29:42 -0700

    Create Java Shell Project

---
+++ CS330/Projects/Java Shell/src/Shell.java
@@ -0,0 +1,12 @@
+public class Shell {
+
+	/**
+	 * Insertion Point
+	 * 
+	 * @param args Runtime Arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
